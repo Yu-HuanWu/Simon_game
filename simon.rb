51dd5c4bfa3d467378d5042fce2623406464d6c0
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-07-27 23:45:55 -0700

    added timer and clear the terminal for each show sequence, also player's score

--- simon.rb
+++ simon.rb
@@ -16,11 +16,24 @@ class Simon
       self.take_turn
     end
     self.game_over_message
-    self.reset_game
+    puts "Wanna go again? Y/N".blue
+    input= gets.chomp
+    if input.upcase == "Y"
+      self.reset_game
+    end
   end
 
   def take_turn
       self.show_sequence
+      puts "Ready?"
+      sleep 1
+      puts "3"
+      sleep 1
+      puts "2"
+      sleep 1
+      puts "1!"
+      sleep 1
+      system("clear")
       self.require_sequence
       if !@game_over
         self.round_success_message
@@ -38,7 +51,7 @@ class Simon
     while i< @seq.length
       puts "Enter either red green blue or yellow in the correct sequence, 1 color at a time"
       input= gets.chomp
-      if input != @seq[i]
+      if input.downcase != @seq[i]
         @game_over=true
         break
       else
@@ -52,11 +65,12 @@ class Simon
   end
 
   def round_success_message
-    puts "You did it kid, didn't think you got it in you.".green
+    puts "You did it kid, didn't think you had it in you.".green
   end
 
   def game_over_message
     puts "Game over man, game over!".red
+    puts "Your score is #{@sequence_length-1}!".yellow.on_blue.blink
   end
 
   def reset_game
